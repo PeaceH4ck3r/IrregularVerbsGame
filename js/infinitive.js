208022f6f2062e9720852b9632b8f2e62a4f3c78
PeaceH4ck3r <peacehacker@outlook.es>
2016-06-21 17:55:50 +0200

    Hola

---
+++ js/infinitive.js
@@ -0,0 +1,39 @@
+$('.contenedor>p.t').prop('innerHTML', 'Infinitivo del verbo: <b></b>');
+
+$.get('php/infinitive.php', imprimir);
+
+function imprimir(data){
+    var datos = data.split('/:/');
+    
+    n1 = Math.floor(Math.random() * 3);
+    do {
+        n2 = Math.floor(Math.random() * 3);
+    } while(n2 == n1);
+    
+    do {
+        n3 = Math.floor(Math.random() * 3);
+    } while(n2 == n3 || n3 == n1);
+    
+    
+    $('.contenedor>p.t>b').prop('innerHTML', datos[0]);
+    
+    c1 = $('.contenedor>p.o:eq('+n1+')');
+    c1.prop('innerHTML', '');
+    c1.append(datos[1]);
+    c1.addClass('well');
+    
+    c2 = $('.contenedor>p.o:eq('+n2+')');
+    c2.prop('innerHTML', '');
+    c2.append(datos[2]);
+    
+    c3 = $('.contenedor>p.o:eq('+n3+')');
+    c3.prop('innerHTML', '');
+    c3.append(datos[3]);
+}
+
+$('.contenedor>a').click(function(e){
+    e.preventDefault();
+    $('.o').css('background-color', '#1565c0');
+    $('.o').removeClass('well');
+    $.get('php/infinitive.php', imprimir);
+})
---
+++ php/pastParticiple.php
@@ -0,0 +1,35 @@
+<?php
+    require('conexion.php');
+
+    $randomNumber = mt_rand(1, 51);
+    
+    //RANDOM
+    if($randomNumber%2 == 0){
+        $consulta = 'SELECT PASTSIMPLE FROM verbos WHERE ID ='.$randomNumber;
+    } else {
+        $consulta = 'SELECT INFINITIVE FROM verbos WHERE ID ='.$randomNumber;
+    }
+    $resultados = mysqli_query($conexion, $consulta);
+    $verbo = mysqli_fetch_row($resultados);
+    $verbo = strtoupper($verbo[0]);
+
+    //CORRECTO
+    $consulta = 'SELECT PASTPARTICIPLE FROM verbos WHERE ID ='.$randomNumber;
+    $resultados = mysqli_query($conexion, $consulta);
+    $correcto = mysqli_fetch_row($resultados);
+    $correcto = strtoupper($correcto[0]);
+
+    //OPCION1
+    $consulta = 'SELECT OP1 FROM trampa WHERE ID ='.$randomNumber;
+    $resultados = mysqli_query($conexion, $consulta);
+    $opcion1 = mysqli_fetch_row($resultados);
+    $opcion1 = strtoupper($opcion1[0]);
+
+    //OPCION 2
+    $consulta = 'SELECT OP2 FROM trampa WHERE ID ='.$randomNumber;
+    $resultados = mysqli_query($conexion, $consulta);
+    $opcion2 = mysqli_fetch_row($resultados);
+    $opcion2 = strtoupper($opcion2[0]);
+
+    echo $verbo.'/:/'.$correcto.'/:/'.$opcion1.'/:/'.$opcion2;
+?>
